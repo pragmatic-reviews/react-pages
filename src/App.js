1bf5f0560161a6df09433d22b15d36fd7ad6632d
pragmaticreviews <info@pragmaticreviews.com>
2021-04-11 14:49:00 -0300

    Changing message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>TESTING GITHUB PAGES WITH REACT AND GITHUB ACTIONS</h1>
         </p>
         <a
           className="App-link"
